f2b1287259cd0ebf64f62b631e59c0ca8fce1c01
Shaurya Sinha <shaurya157@gmail.com>
2016-09-15 16:28:17 -0700

    created grid, populated bombs, and stored bomb locations

--- game.rb
+++ game.rb
@@ -1,2 +1,31 @@
 class Game
+
+  def self.mine_field(number = 9)
+    grid = Array.new(number) { Array.new(9) { Tile.new } }
+
+    Game.new(grid)
+  end
+
+  def initialize(grid)
+    @grid = grid
+  end
+
+  def populate_bombs
+    @grid.length.times do
+      row, col = randomize
+      tile = @grid[row][col]
+
+      until !(tile.value == 0)
+        row, col = randomize
+        tile = @grid[row][col]
+      end
+      tile.value = "☼"
+    end
+  end
+
+  def randomize
+    row = rand(@grid.length)
+    col = rand(@grid.length)
+    [row, col]
+  end
 end
--- tile.rb
+++ tile.rb
@@ -1,2 +1,19 @@
 class Tile
+  attr_accessor :value, :state
+  def initialize
+    @value = 0
+    @state = false
+  end
+
+  def flag
+    @state = nil
+  end
+
+  def unflag
+    @state = false
+  end
+
+  def reveal
+    @state = true
+  end
 end
